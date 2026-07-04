00f926113e600479249c8558d039dc467871c681
Daniel Schiemann <dschiemann75@gmail.com>
2026-07-04 12:21:31 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,4 @@
-import com.google.gms.googleservices.GoogleServicesPlugin.MissingGoogleServicesStrategy
+
 
 plugins {
   alias(libs.plugins.android.application)
@@ -14,7 +14,7 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.aistudio.erna_assistant.bcrkly"
+    applicationId = "dynasync.de"
     minSdk = 24
     targetSdk = 36
     versionCode = 1
